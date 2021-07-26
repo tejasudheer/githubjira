e9ee31f8262a4951b5f2dcefc7550a7ba28b4a85
teju <tejaswini199427@gmail.com>
2021-07-26 18:07:34 +0530

    hello everyone

---
+++ a - Copy (2).java
@@ -0,0 +1,10 @@
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
---
+++ a.java
@@ -0,0 +1,64 @@
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
+
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
+
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
+
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
+
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+} 
+/**
+* This program is used to print "Hello World".
+* @author W3spoint
+*/
+public class HelloWorld {
+public static void main(String args[]){
+//This line will print "Hello World".
+System.out.println("Hello World.");
+}
+}
